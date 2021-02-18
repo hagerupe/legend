217749e9b1f4eb17b22f24a4d88f4a7179e7d756
hagere <erik.hagerup@gs.com>
2021-02-17 20:24:59 -0800

    Auto create gitlab app

--- installers/aws-eks-cdk/lib/handlers/gitlabApplicationConfig/curl.sh
+++ installers/aws-eks-cdk/lib/handlers/gitlabApplicationConfig/curl.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-set -x
-set -e
-
-gitlab_host="https://gitlab.sky-hagere.io"
+gitlab_host="$1"
 gitlab_user="root"
-gitlab_password="8296daf8-6fb6-11eb-9439-0242ac130002"
+gitlab_password="$2"
 
 # curl for the login page to get a session cookie and the sources with the auth tokens
-body_header=$(curl -L -c cookies.txt -i "${gitlab_host}/users/sign_in" -s)
+body_header=$(curl -s -L -c /tmp/cookies.txt -i "${gitlab_host}/users/sign_in" -s)
 
 # grep the auth token for the user login for
 #   not sure whether another token on the page will work, too - there are 3 of them
 csrf_token=$(echo $body_header | perl -ne 'print "$1\n" if /new_user.*?authenticity_token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
 
 # send login credentials with curl, using cookies and token from previous request
-curl -L -b cookies.txt -c cookies.txt -i "${gitlab_host}/users/sign_in" \
+login=$(curl -s -L -b /tmp/cookies.txt -c /tmp/cookies.txt -i "${gitlab_host}/users/sign_in" \
     --data "user[login]=${gitlab_user}&user[password]=${gitlab_password}" \
-    --data-urlencode "authenticity_token=${csrf_token}"
+    --data-urlencode "authenticity_token=${csrf_token}")
 
 # send curl GET request to personal access token page to get auth token
-body_header=$(curl -L -H 'user-agent: curl' -b cookies.txt -i "${gitlab_host}/profile/personal_access_tokens" -s)
+body_header=$(curl -L -H 'user-agent: curl' -b /tmp/cookies.txt -i "${gitlab_host}/profile/personal_access_tokens" -s)
 csrf_token=$(echo $body_header | perl -ne 'print "$1\n" if /authenticity_token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
 
 # curl POST request to send the "generate personal access token form"
 # the response will be a redirect, so we have to follow using `-L`
-body_header=$(curl -L "${gitlab_host}/-/profile/personal_access_tokens" \
+body_header=$(curl -s -L "${gitlab_host}/-/profile/personal_access_tokens" \
   -H 'authority: gitlab.sky-hagere.io' \
   -H 'pragma: no-cache' \
   -H 'cache-control: no-cache' \
@@ -39,11 +36,11 @@ body_header=$(curl -L "${gitlab_host}/-/profile/personal_access_tokens" \
   -H 'sec-fetch-dest: document' \
   -H "referer: ${gitlab_host}/-/profile/personal_access_tokens" \
   -H 'accept-language: en-US,en;q=0.9,de;q=0.8' \
-  -b cookies.txt \
+  -b /tmp/cookies.txt \
   --data-urlencode "authenticity_token=${csrf_token}" \
   --data 'personal_access_token[name]=golab-generated&personal_access_token[expires_at]=&personal_access_token[scopes][]=api')
 
 # Scrape the personal access token from the response HTML
 personal_access_token=$(echo $body_header | perl -ne 'print "$1\n" if /created-personal-access-token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
 
-echo $personal_access_token
+curl --request POST --header "PRIVATE-TOKEN: ${personal_access_token}" --data "name=Legend Demo2&redirect_uri=http://redirect.uri&scopes=" "${gitlab_host}/api/v4/applications"
--- installers/aws-eks-cdk/lib/handlers/gitlabApplicationConfig/index.py
+++ installers/aws-eks-cdk/lib/handlers/gitlabApplicationConfig/index.py
@@ -2,11 +2,18 @@ def main(event, context):
   import logging as log
   import boto3
   import hashlib
-
-  log.getLogger().setLevel(log.INFO)
-
   import os
-
+  import json
   import subprocess
 
-  log.info(subprocess.check_output(["curl.sh"]))
+  log.getLogger().setLevel(log.INFO)
+  client = boto3.client('secretsmanager')
+
+  gitlab_host="https://gitlab.sky-hagere.io"
+  secret_id = "GitlabRootPassword100B7897-w2dXte30COa9"
+
+  gitlab_password = client.get_secret_value(SecretId=secret_id)['SecretString']
+  application_json = subprocess.check_output(["./curl.sh", gitlab_host, gitlab_password]).decode("utf-8")
+  application = json.loads(application_json)
+  application_id = application['application_id']
+  secret = application['secret']
